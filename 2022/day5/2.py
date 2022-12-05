bbf4bb8dc250c0a68e147a81317f5a60bc01be65
Tambet Liiv <tambet.liiv@rangeforce.com>
2022-12-05 10:23:46 +0000

    add day5

---
+++ 2022/day5/2.py
@@ -0,0 +1,37 @@
+file = open("input.txt", "r")
+
+stacks = {}
+
+helper = []
+
+for line in file:
+  if line.strip() == "":
+    break
+  helper.append(line.rstrip('\n'))
+
+s = helper.pop()
+
+helper.reverse()
+
+for i in range(0, len(s), 4): 
+  stack_index = int(s[i:i+4].strip())
+  stacks[stack_index] = []
+  for candidate in helper:
+    crate = candidate[i:i+4].strip().strip(']').strip('[')
+    if crate:
+      stacks[stack_index].append(crate)
+
+for line in file:
+  move = line.strip()
+  times = int(move.split()[1])
+  from_stack = int(move.split()[3])
+  to_stack = int(move.split()[5])
+  for i in range(times, 0, -1):
+    crate = stacks[from_stack].pop(-i)
+    stacks[to_stack].append(crate)
+
+file.close()
+
+for i in range(1, len(stacks)+1):
+  print(stacks[i].pop(), end='')
+
